c0e1899e21140dc9c5d62dac6c4091df441bae6f
Kazu Takahashi <num96kazu19@icloud.com>
2022-02-15 22:15:49 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const r = Math.random() * 256;
-  const g = Math.random() * 256;
-  const b = Math.random() * 256;
-  console.log(r, g, b);
+  const [color1, setColor1] = useState(0);
+  const [color2, setColor2] = useState(0);
+  const [color3, setColor3] = useState(0);
+
+  const num = () => {
+    return Math.floor(Math.random() * 256);
+  };
+
+  const changeColor = () => {
+    setColor1(num);
+    setColor2(num);
+    setColor3(num);
+    document.body.style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
+  };
 
   return (
-    <>
-      <p className="color">RGB</p>
-      <button className="btn">Change BG Color</button>
-    </>
+    <div>
+      <p className="color">
+        RGB({color1}, {color2}, {color3})
+      </p>
+      <button className="btn" onClick={changeColor}>
+        Change BackGround Color
+      </button>
+    </div>
   );
 }
 
